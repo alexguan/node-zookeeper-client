419af83457b2b0e5bb6b41711b58c02e1bd3f5e4
Alex Guan <alex.guan@gmail.com>
2013-04-24 18:08:10 -0700

    Add unit test for State.js

---
+++ test/lib/State.test.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2013 Yahoo! Inc. All rights reserved.
+ */
+
+/*global describe, it, beforeEach, before, after */
+
+var expect = require('chai').expect;
+var State = require('../../lib/State.js');
+
+
+describe('State', function () {
+    describe('constants', function () {
+        it('should have all defined states', function () {
+            expect(State.SYNC_CONNECTED).to.exist;
+            expect(State.DISCONNECTED).to.exist;
+            expect(State.AUTH_FAILED).to.exist;
+            expect(State.CONNECTED_READ_ONLY).to.exist;
+            expect(State.SASL_AUTHENTICATED).to.exist;
+            expect(State.EXPIRED).to.exist;
+        });
+    });
+});
+
