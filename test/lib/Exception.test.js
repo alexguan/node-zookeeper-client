b71072201b9557677106f0737b046857fc84b251
Alex Guan <alex.guan@gmail.com>
2013-05-07 16:56:37 -0700

    Update license statement in test files.

--- test/lib/Exception.test.js
+++ test/lib/Exception.test.js
@@ -1,5 +1,8 @@
 /**
  * Copyright (c) 2013 Yahoo! Inc. All rights reserved.
+ *
+ * Copyrights licensed under the MIT License. See the accompanying LICENSE file
+ * for terms.
  */
 
 /*global describe, it, beforeEach, before, after */
